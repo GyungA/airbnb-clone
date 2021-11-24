26d0aa7e11887a33e6ca4d602b7ac8b8b11f12ba
GyungA <gga612@naver.com>
2021-11-24 14:39:03 +0900

    #8.8 Intercepting Model save() and admin_save(): super().save(*args, **kwargs)

--- rooms/models.py
+++ rooms/models.py
@@ -66,6 +66,10 @@ class Room(core_models.TimeStampedModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        self.city = self.city.title()
+        super().save(*args, **kwargs)
+
     def total_rating(self):
         try:
             all_reviews = self.reviews.all()
