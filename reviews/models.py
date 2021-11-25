d26c6beb4c32f9f7ed5915c40721c3444af9eccf
GyungA <gga612@naver.com>
2021-11-25 12:25:14 +0900

    #9.6 seed_reviews: seeder creates 'review' with textfield automatically

--- reviews/models.py
+++ reviews/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from core import models as core_models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Review(core_models.TimeStampedModel):
     """ Review Model Definition """
     
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    communication = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    cleanliness = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    location = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    check_in = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     user = models.ForeignKey("users.User", related_name="reviews", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", related_name="reviews", on_delete=models.CASCADE)
 
