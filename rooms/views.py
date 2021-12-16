bd043abc16adb49959cfa344806714a1005dd9fc
GyungA <gga612@naver.com>
2021-12-16 15:58:19 +0900

    #13.4 Finishing the Form: django remember selected amenities and facilities (use 'getlist')

--- rooms/views.py
+++ rooms/views.py
@@ -30,8 +30,10 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    selected_amenities = request.GET.get("amenities")
-    selected_facilities = request.GET.get("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
 
     form = {
             "city": city,
@@ -42,6 +44,10 @@ def search(request):
             "bedrooms": bedrooms,
             "beds": beds,
             "baths": baths,
+            "selected_amenities": selected_amenities,
+            "selected_facilities": selected_facilities,
+            "instant": instant,
+            "super_host": super_host,
     }
 
     room_types = models.RoomType.objects.all()
