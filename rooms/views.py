36b221061025bf6973c399c5353286aee50d507a
GyungA <gga612@naver.com>
2021-12-16 09:53:45 +0900

    #12.0 URLs and Arguments: use name, app_names, namespace to separate urls

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,8 @@
-from django.core.paginator import EmptyPage
+from django.utils import timezone
 from django.views.generic import ListView
-from django.shortcuts import redirect
+from django.shortcuts import render
 from . import models
+
 
 class HomeView(ListView):
 
@@ -10,3 +11,14 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+
+def room_detail(request, pk):
+    return render(request, "rooms/detail.html")
