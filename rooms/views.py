181df55e05abb4296ef3643a3e6da13e7338ab99
GyungA <gga612@naver.com>
2021-12-16 10:36:05 +0900

    #12.2 room_detail FBV finished

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -13,12 +14,10 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
-
 
 def room_detail(request, pk):
-    return render(request, "rooms/detail.html")
+    try: 
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
