cdd184e180a1cc4e49af1fbc74c07d6250484ef2
GyungA <gga612@naver.com>
2021-12-16 13:53:12 +0900

    #13.1 Starting the Form: created search page

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django_countries import countries
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from . import models
@@ -20,6 +21,8 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    city = request.GET.get("city")
+    city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
-    return render(request, "rooms/search.html", {"city": city})
+    room_types = models.RoomType.objects.all()
+    return render(request, "rooms/search.html", {"city": city, "countries": countries, "room_types": room_types},)
+    
