e463472cfe0b73601347f7dd468ec6e5ce4a56d7
GyungA <gga612@naver.com>
2021-12-22 22:12:28 +0900

    #15.2 ModelForms are AWESOME: change SignUpForm(forms.Form) to SignUpForm(forms.ModelForm), using django data(first_name, last_name), commit=False

--- users/models.py
+++ users/models.py
@@ -33,10 +33,10 @@ class User(AbstractUser):
     bio = models.TextField(blank=True)
     birthdate = models.DateField(null=True, blank=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, blank=True
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
     )
     currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, blank=True
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
     )
     superhost = models.BooleanField(default=False)
 
